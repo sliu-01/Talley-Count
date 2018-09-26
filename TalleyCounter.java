bbd86e65716b15e5302c8a1896d4d748545ab8b8
stevenliu-sl <stevenliu.sl01@gmail.com>
2018-09-25 23:11:38 -0400

    Homework from 9/25 - made methods for the TalleyCounter

--- TalleyCounter.java
+++ TalleyCounter.java
@@ -1,9 +1,46 @@
 
-public class TalleyCounter {
+public class TalleyCounter 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int count;
+	
+	public TalleyCount()
+	{
+		count = 0;
+	}
+	public void click()
+	{
+		count = count + 1;
+		//count = count++
+		//count += 1
+		
+	}
+	
+	public void initialValue(startingNum)
+	{
+	
+		count = startingNum;
+	
 	}
 
+	public void unclick()
+	{
+		
+		count = count - 1;
+		
+	}
+	
+	public void reset()
+	{
+		
+		count = 0;
+	
+	}
+		
+	public int getCount()
+	{
+		
+		return count;
+		
+	}
 }
